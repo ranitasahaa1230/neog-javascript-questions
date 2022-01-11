eed42fca8b27d9bbe5819375f4a848a6fa5b6946
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2022-01-11 21:31:00 +0530

    update

---
+++ es6+/func-medium.js
@@ -0,0 +1,30 @@
+// 1.Given an array, your function should return the length of the array.
+// Example:
+// Input: arrayLength([1,5,3,7,8]) ––> Output: 5
+
+
+
+// 2.Given an array and an item, your function should return the index at which the item is present.
+// Example:
+// Input: indexOf([1,6,3,5,8,9], 3) ––> Output: 2
+
+
+
+//3. Given an array and two numbers, your function should replace all entries of first number in an array with the second number.
+// Example:
+// Input: replace([1,5,3,5,6,8], 5, 10) ––> Output: [1,10,3,10,6,8]
+
+
+//4. Given two arrays, your function should return single merged array.
+// Example:
+// Input: mergeArray([1,3,5], [2,4,6]) ––> Output: [1,3,5,2,4,6]
+
+
+// 5. Given a string and an index, your function should return the character present at that index in the string.
+// Example:
+// Input: charAt("neoGcamp", 4) ––> Output: c
+
+
+// 6. Given two dates, your function should return which one comes before the other.
+// Example:
+// Input: minDate('02/05/2021', '24/01/2021') ––> Output: 24/01/2021
---
+++ es6+/practice-assign.js
@@ -0,0 +1,178 @@
+// 1. var aloo = 1;
+// if (aloo == 1) {
+//    var a = 2;
+//    console.log(a);
+// }
+// console.log(aloo);
+
+//ES6
+// const aloo=1;
+// if(aloo){
+//     const a=2;
+//     console.log(a);
+// }
+// console.log(aloo);
+// ------------------------------------------------------------------
+
+// 2. var multiply = function(x, y) {
+//     return x * y;
+//   };
+// console.log(multiply(2,3))
+
+//ES6
+// const multiply=(x,y)=>x*y;
+// console.log(multiply(2,3))
+// ----------------------------------------------------------------------
+
+// var customer = {
+//     name: "Bhaalo"
+//   };
+//   var card = {
+//     amount: 20,
+//     product: "Aaalo",
+//     unitprice: 50
+//   };
+//   var message = "Hello " + customer.name + " wants to buy " + card.amount +" " + card.product + " for price of " + card.unitprice + " per piece"
+//   console.log(message)
+
+  //ES6
+//   const customer = {
+//     name: "Bhaalo"
+//   };
+//   const card = {
+//     amount: 20,
+//     product: "Aaalo",
+//     unitprice: 50
+//   };
+//   const message=`Hello ${customer.name} wants to buy ${card.amount} ${card.product} for price of ${card.unitprice} per piece`;
+//   console.log(message)
+// -----------------------------------------------------------------------------------------------------------------------------
+
+// var Neog = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"],
+// CEO = Neog[0],
+// Mentor = Neog[2];
+
+//ES6
+// const [CEO,Mentor] = ["Tanvi", "Swap", "Tanay", "MA", "CA", "PA", "TA"];
+// ----------------------------------------------------------------------------------------------------------
+
+// var arr = ["MA", "TA", "PA", "CA"];
+// var firstName = arr[0];
+// var surname = arr[1];
+// console.log(firstName);
+// console.log(surname);
+
+//ES6
+// const [firstName, surname] = ["MA", "TA", "PA", "CA"];
+// console.log(`${firstName}`);
+// console.log(surname);
+// --------------------------------------------------------------------------------------------
+
+// var Aaloo = "Tasty";
+// var Bhaloo = "Cute";
+// var Obj = {
+//   Aaloo: Aaloo,
+//   Bhaloo: Bhaloo
+// };
+
+//ES6
+// const Aaloo = "Tasty";
+// const Bhaloo = "Cute";
+// const Obj = {
+//   Aaloo,  //shorthand prop
+//   Bhaloo
+// };
+// console.log(Obj)
+// ------------------------------------------------------------------
+
+// var a = 5;
+// var b = 10;
+// console.log("Fifteen is ".concat(a + b, " and n0t ").concat(2 * a + b, "."));
+
+//ES6
+// const a = 5;
+// const b = 10;
+// console.log(`Fifteen is ${a + b} and not ${2*a+b}.`);
+// --------------------------------------------------------------------------
+
+// var arithmeticsObj = {
+//     sum: function sum(num1, num2) {
+//       return num1 + num2;
+//     },
+//     multiply: function multiply(num1, num2) {
+//       return num1 * num2;
+//     }
+//   };
+
+//ES6 
+// const arithmeticsObj={
+//     sum:(n1,n2)=>n1+n2,
+//     multiply:(n1,n2)=>n1*n2
+//   }
+//   console.log(arithmeticsObj.sum(2,3))
+//   console.log(arithmeticsObj.multiply(2,3))
+//   -----------------------------------------------------------------------------------
+
+// var avengers = {
+//     operation: "Assemble",
+//     members: [
+//       {
+//         ironMan: "Tony Stark"
+//       },
+//       {
+//         captainAmerica: "Steve Rogers"
+//       },
+//       {
+//         blackWidow: "Natasha Romanoff"
+//       }
+//     ]
+//   };
+//   var operation = avengers.operation,
+//     members = avengers.members;
+
+//ES6
+// const avengers = {
+//     operation: "Assemble",
+//     members: [
+//       {
+//         ironMan: "Tony Stark"
+//       },
+//       {
+//         captainAmerica: "Steve Rogers"
+//       },
+//       {
+//         blackWidow: "Natasha Romanoff"
+//       }
+//     ]
+//   };
+//   const [operation,members] = avengers;
+//   ---------------------------------------------------------------------------------------------
+
+// const packIt = (...args) => console.log(args)
+// packIt(1,2,3,5,5)
+
+//ES5
+// function packIt(args){
+//     console.log(args)
+// }
+// packIt(1,2,3,5,5)
+// ----------------------------------------------------------------------
+
+//output
+// const hello = () => "Hello"
+// const welcome = () => "Welcome"
+// const [Hello = hello(), Welcome = welcome()] = ["Namaste"];
+// console.log(Hello, Welcome) //Namaste Welcome
+// ---------------------------------------------------------------------------
+
+//output
+const obj = {
+    aloo : 1,
+    bhallo : 2
+}
+
+const {c : aloo = [2,3,4].push(5), aloo} = obj 
+
+console.log(aloo) //err
+
+
