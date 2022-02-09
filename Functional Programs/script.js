7616adec572b3c4741b713a506ae4b5006d46abd
Ranita Saha <saharanitaa1230@gmail.com>
2022-02-09 13:29:53 +0530

    Update script.js

--- Functional Programs/script.js
+++ Functional Programs/script.js
@@ -15,6 +15,25 @@ const nos=[1,7,8,10,45,99,70];
 // const moreThanTen=(num)=>num>10;
 // console.log(nos.filter(moreThanTen));
 
-const nosPlus=nos.map(num=>num+2);
-console.log(nosPlus);
+// const nosPlus=nos.map(num=>num+2);
+// console.log(nosPlus);
 
+const arr=[1,3,5,99,11,12];
+// const sumLessTen=arr.reduce((acc,cval)=>{
+//     cVal=acc+cval;
+//     if(cval<10)
+//         return cVal;
+//     else
+//         return acc;
+// },0)
+// console.log(sumLessTen);
+
+const arr1=[1,3,5,2,22,11,9];
+const oddSum=(sum,num)=>num%2===0 ? sum: sum+num;
+// console.log(arr1.reduce(oddSum,0));
+
+const reduceObj=(oddEvenObj,num)=>num%2===0 ? {...oddEvenObj,even:oddEvenObj.even+num} :{...oddEvenObj,odd:oddEvenObj.odd+num};
+const oddEvenObj={even:0,odd:0}
+// console.log(reduceObj(oddEvenObj,2))
+// console.log(reduceObj(oddEvenObj,3))
+console.log(arr1.reduce(reduceObj,oddEvenObj));
