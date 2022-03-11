8ba3e7dacba8432a72d1227aef495f3ce9c066bb
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-11 23:55:42 +0530

    Create numbers.js

---
+++ es6+/numbers.js
@@ -0,0 +1,18 @@
+// Write a program to input 2 numbers and display the sum of the numbers to the console.
+const a=20;
+const b="40";
+const sum=Number(20+40);
+// console.log(`Sum is ${sum}`)
+
+// Write a JavaScript program to calculate the simple interest given P,R,T with the given formula. 
+// Formula: SI = (P * T * R) / 100 Where: P = principal amount T = time R = rate SI = simple interest
+const P=100;
+const R=0.06;
+const T=2;
+const SI = (P * T * R) / 100;
+// console.log(`SI is ${SI}`)
+
+// Write a program to calculate the kinetic energy of a body with mass 'm' and volume 'v'.
+const m=10, v=12;
+const KE=0.5 * m * v * v;
+// console.log(`KE is ${KE}`)
