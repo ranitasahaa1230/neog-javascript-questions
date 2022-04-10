cabc7433d38fe70e4ea388aa92ed5025d608e2b8
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-10 17:04:11 +0530

    Update index.js

--- Practice-NEOG/index.js
+++ Practice-NEOG/index.js
@@ -73,10 +73,43 @@ const characters2 = [
     },
     ];
     const char=characters2.every((ch)=>ch.height<=200)
-    console.log(char) //false
+    // console.log(char) //false
 
     const char2=characters2.some((ch)=>ch.eye_color==='blue')
     // console.log(char2) //true
 
     const char3=characters2.find((ch)=>ch.eye_color==='blue')
     // console.log(char3)
+
+    const characters52 = [
+        {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+        },
+        {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+        },
+        {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+        },
+        {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+        },
+        ]
+        const chars=characters52.reduce((acc,cval)=>cval.gender==="male" ? [...acc,{...cval}]: acc, [])
+        console.log(chars)
